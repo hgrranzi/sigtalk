c8d6dcb1b47da37ced6d1ba2643a5761fd49e379
hgrranzi <vkh.mariia@gmail.com>
2021-05-27 11:27:05 +0300

    Created the header

---
+++ sigtalk.h
@@ -0,0 +1,14 @@
+/* *********************************************** */
+/*                                                 */
+/*       by: hgrranzi <vkh.mariia@gmail.com>       */
+/*                                                 */
+/* *********************************************** */
+
+#ifndef SIGTALK_H
+# define SIGTALK_H
+
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+#endif
