486623e1a47a23b0935c7accc265ee56d724da31
hgrranzi <vkh.mariia@gmail.com>
2021-05-27 18:10:57 +0300

    Added aka_isdigit and aka_atoi functions

--- sigtalk.h
+++ sigtalk.h
@@ -11,8 +11,12 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+# define INT_MAX 2147483647
+
+int		aka_isdigit(char c);
 void	aka_putchar(char c);
 void	aka_putnbr(int nbr);
+int		aka_atoi(char *str);
 
 void	handle_signal(int sig_number);
 
--- sigtalk_utils.c
+++ sigtalk_utils.c
@@ -6,6 +6,11 @@
 
 #include "sigtalk.h"
 
+int	aka_isdigit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
 void	aka_putchar(char c)
 {
 	write(1, &c, 1);
@@ -24,4 +29,23 @@ void	aka_putnbr(int nbr)
 			aka_putnbr(nbr / 10);
 		aka_putchar(nbr % 10 + 48);
 	}
+}
+
+int	aka_atoi(char *str)
+{
+	long	nbr;
+	long	sign;
+	int		i;
+
+	nbr = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] && aka_isdigit(str[i]) && nbr <= INT_MAX)
+	{
+		nbr = nbr * 10 + str[i] - '0';
+		i++;
+	}
+	if (!i || str[i] != '\0' || nbr > INT_MAX)
+		sign = -1;
+	return ((int)(nbr * sign));
 }
