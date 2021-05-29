2291212b8f0599019ab7a18c61491fdcbad3183b
hgrranzi <vkh.mariia@gmail.com>
2021-05-29 18:09:38 +0300

    Updated the sigtalk.h

--- sigtalk.h
+++ sigtalk.h
@@ -25,17 +25,21 @@ typedef struct s_data
 	int				index;
 }					t_data;
 
-int		aka_isdigit(char c);
-void	aka_putchar(char c);
-void	aka_putnbr(int nbr);
-size_t	aka_strlen(const char *s);
-int		aka_atoi(char *str);
+int				aka_isdigit(char c);
+void			aka_putchar(char c);
+void			aka_putnbr(int nbr);
+size_t			aka_strlen(const char *s);
+int				aka_atoi(char *str);
 
-void	handle_signal(int sig_number);
+void			handle_signal(int sig_number);
+void			take_int(t_data *data, int *what_take);
+void			take_char(t_data *data);
+unsigned char	*create_str(int str_len);
+void			reset_data(t_data *data);
 
-pid_t	check_pid(char *arg_pid);
-void	send_message(pid_t server_pid, char *message);
-void	send_int(pid_t server_pid, int nbr);
-void	send_char(pid_t server_pid, unsigned char c);
+void			send_message(pid_t server_pid, char *message);
+void			send_int(pid_t server_pid, int nbr);
+void			send_char(pid_t server_pid, unsigned char c);
+void			recieved(int sig_number);
 
 #endif
